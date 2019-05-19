045748626dfdb60f3d0fd52f4971d84abbbca8c3
David Xu <david.xu@spark.co.nz>
2019-05-20 01:09:22 +1200

    Upgraded project to using reakit 1.0

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { Button, styled } from 'reakit';
+import { Button } from 'reakit';
+import styled from 'styled-components';
 import  { backgroundColor, textColor, borderColor } from '../SETThemeProvider';
 
 
@@ -63,7 +64,7 @@ const TertiaryButton = styled(StyledButton)`
   ${backgroundColor.base.white};
 
 
-    
+
   ${states(['hover', 'active'])} {
     ${textColor.interation.dark};
     ${backgroundColor.base.white};
@@ -74,7 +75,7 @@ const TertiaryButton = styled(StyledButton)`
     }
   }
 
-  
+
   border: none;
 
 
@@ -84,7 +85,7 @@ const TertiaryButton = styled(StyledButton)`
     transition: transform 1s;
     transform: translateX(0px);
     content: '';
-    background-image: url("data:image/svg+xml,%3Csvg width='32' height='32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Cdefs%3E%3Cpath id='a' d='M9.429 32l-2.922-2.922L19.584 16 6.507 2.922 9.429 0l16 16z'/%3E%3C/defs%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cmask id='b' fill='%23fff'%3E%3Cuse xlink:href='%23a'/%3E%3C/mask%3E%3Cuse fill='%23000' fill-rule='nonzero' xlink:href='%23a'/%3E%3Cg mask='url(%23b)' fill='%23${blue}'%3E%3Cpath d='M0 0h32v32H0z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");  
+    background-image: url("data:image/svg+xml,%3Csvg width='32' height='32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Cdefs%3E%3Cpath id='a' d='M9.429 32l-2.922-2.922L19.584 16 6.507 2.922 9.429 0l16 16z'/%3E%3C/defs%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cmask id='b' fill='%23fff'%3E%3Cuse xlink:href='%23a'/%3E%3C/mask%3E%3Cuse fill='%23000' fill-rule='nonzero' xlink:href='%23a'/%3E%3Cg mask='url(%23b)' fill='%23${blue}'%3E%3Cpath d='M0 0h32v32H0z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
     height: 14px;
     width: 14px;
     background-size: contain;
