f151846d56a61dc4a7d06d5ef693f222e489bca3
David Xu <T821829@spark.co.nz>
2019-08-27 17:34:00 +1200

    Adding role, title, and aria label to icons

--- src/components/FontIcons/FontIcons.tsx
+++ src/components/FontIcons/FontIcons.tsx
@@ -1,46 +1,41 @@
+import * as React from 'react';
 import styled, { css } from 'styled-components';
-import React from 'react';
 import { prop } from 'styled-tools';
 
-interface IconProps extends JSX.Element {
-    name?: string,
-    className?: string,
+interface BaseFontIconProps {
+    inline?: boolean;
+    size?: string;
+    color?: string;
+    cursor?: string;
+    marginTop?: string;
+    marginRight?: string;
+    marginBottom?: string;
+    marginLeft?: string;
+    margin?: string;
+    hoverColor?: string;
+    role: string;
 }
 
-interface BaseIconProps extends JSX.Element {
-    inline?: boolean,
-    size?: string,
-    color?: string,
-    cursor?: string,
-    marginTop?: string,
-    marginRight?: string,
-    marginBottom?: string,
-    marginLeft?: string,
-    margin?: string,
-    hoverColor?: string,
-    className?: string
-}
-
-const BaseFontIcon = styled.span<BaseIconProps>`
-    font-family: 'spark-icon-family';
+const BaseFontIcon = (styled.span) < BaseFontIconProps >`
+    font-family: 'spark-icon-family', arial, sans-serif;
     speak: none;
     font-style: normal;
     font-weight: normal;
     font-variant: normal;
-    display: ${({inline}) => (inline ? 'inline' : 'block')};
+    display: ${props => (props.inline ? 'inline' : 'block')};
     background-color: transparent;
     text-decoration: none;
     padding: 0;
     width: ${({ size }) => size || '16px'};
     height: ${({ size }) => size || '16px'};
     font-size: ${({ size }) => size || '16px'};
-    color: ${({ color }) => prop(color, '')};
+    color: ${({ color }) => prop(color || 'theme.text-color.text-grey-5')};
     cursor: ${({ cursor }) => cursor || 'pointer'};
+    margin: ${({ margin }) => margin || ''};
     margin-top: ${({ marginTop }) => marginTop || '0'};
     margin-right: ${({ marginRight }) => marginRight || '0'};
     margin-bottom: ${({ marginBottom }) => marginBottom || '0'};
     margin-left: ${({ marginLeft }) => marginLeft || '0'};
-    margin: ${({ margin }) => margin || ''};
     line-height: 1;
     ${({ hoverColor }) => hoverColor
         && css`
@@ -50,7 +45,17 @@ const BaseFontIcon = styled.span<BaseIconProps>`
         `};
 `;
 
-const FontIcons : React.FunctionComponent<IconProps> = ({ name, className, ...props }) => {
+interface IIconProps {
+    name: string,
+    ariaLabel: string,
+    title?: string,
+    role?: string;
+    className?: string,
+}
+
+const Icon = ({
+    name, ariaLabel, className, role, title, ...props
+}: IIconProps) => {
     const lowerCaseName = name.toLowerCase();
     const iconClassName = `icon icon-${lowerCaseName}`;
     const classNameExtracted = `${className || ''} ${iconClassName}`;
@@ -63,8 +68,15 @@ const FontIcons : React.FunctionComponent<IconProps> = ({ name, className, ...pr
         );
     }
 
-    return <BaseFontIcon className={finalClassName} {...props} />;
+    return (
+        <BaseFontIcon
+            className={finalClassName}
+            role={role || 'button'}
+            title={title || 'icon'}
+            aria-label={ariaLabel}
+            {...props}
+        />
+    );
 };
 
-/** @component */
-export default FontIcons;
+export default Icon;
